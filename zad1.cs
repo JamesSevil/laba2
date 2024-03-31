9e6ef5d253100f741edc14cad33602e44f40b5bd
James Sevil <jsclassified24@gmail.com>
2024-03-31 22:20:22 +0700

    C#

---
+++ zad1.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main() {
+        string str = "1001tf11flb10001"; // строка
+        Regex regular = new Regex("1(0+)1"); // условие поиска
+        MatchCollection result = regular.Matches(str); // результат
+
+        foreach (Match match in result) {
+            Console.WriteLine(match.Value); // возвращает совпадение с регулярным выражением
+        }
+    }
+}
