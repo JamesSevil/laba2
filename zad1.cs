c9261e1a1d36109cffa6e6fc975e5534dbb9cfa9
James Sevil <jsclassified24@gmail.com>
2024-04-01 17:02:49 +0700

    C#

--- zad1.cs
+++ zad1.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
-class Program
-{
+class Program {
     static void Main() {
         string str = "1001tf11flb10001"; // строка
         Regex regular = new Regex("1(0+)1"); // условие поиска
