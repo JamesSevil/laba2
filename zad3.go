81678056e5662acddc4c37b398e6ac1825a87491
James Sevil <jsclassified24@gmail.com>
2024-04-02 20:15:43 +0700

    Go

--- zad3.go
+++ zad3.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt" // пакет форматирования и ввода/вывода
+	"fmt"
 )
 
-func sumOfDigits(num int) int { // нахождение суммы цифр
+func sumOfDigits(num int) int { // нахождение суммы цифр числа
 	sum := 0
 	for num != 0 {
 		sum += num % 10
@@ -14,11 +14,26 @@ func sumOfDigits(num int) int { // нахождение суммы цифр
 }
 
 func main() {
-	nums := []int{19, 93, 44, 27} // ввод
+	// ввод чисел
+	var n int
+	for {
+		fmt.Print("Введите кол-во чисел (Больше 0): ")
+		fmt.Scan(&n)
+		if n > 0 {
+			break
+		}
+	}
+
+	nums := make([]int, n)
+	fmt.Print("Введите числа: ")
+	for i := 0; i < n; i++ {
+		fmt.Scan(&nums[i])
+	}
+
 	fmt.Print("Числа: ")
-	for i := 0; i < len(nums); i++ { // перебор всех чисел
+	for i := 0; i < len(nums); i++ { // перебор каждого числа
 		if sumOfDigits(nums[i]) < 10 { // проверка условия
-			fmt.Print(nums[i], " ") // вывод подходящих
+			fmt.Print(nums[i], " ") // вывод подходящих чисел
 		}
 	}
 }
