f939dca1e77af120ff53ed46c34c5638f4e67c2f
James Sevil <jsclassified24@gmail.com>
2024-04-02 19:48:23 +0700

    Python

--- zad3.py
+++ zad3.py
@@ -1,11 +1,22 @@
-def sum_of_digits(num): # нахождение суммы цифр числа
+def sum_of_digits(num):
+    # нахождение суммы цифр числа
     sum = 0
     while num != 0:
         sum += num % 10
         num //= 10
     return sum
 
-nums = [19, 93, 44, 27]  # ввод чисел
-for num in nums:  # перебор каждого числа
-    if sum_of_digits(num) < 10:  # проверка условия
-        print(num)  # вывод подходящих чисел
+# ввод чисел
+n = 0
+while n <= 0:
+    n = int(input("Введите кол-во чисел(Больше 0): "))
+    
+nums = []
+print("Введите числа:")
+for i in range(n):
+    nums.append(float(input()))
+    
+print("Числа:")
+for num in nums:
+    if sum_of_digits(num) < 10:
+        print(num, end=" ")
