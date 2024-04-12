244cbd7bfa56b08881b3d85b8d2efe9ed39012a1
James Sevil <jsclassified24@gmail.com>
2024-04-12 11:05:33 +0700

    C++

--- zad1.cpp
+++ zad1.cpp
@@ -1,18 +1,34 @@
 // регулярные выражения
 #include <iostream>
-#include <regex>
+#include <string>
 
 using namespace std;
 
 int main() {
     
-    string str = "1001tf11flb10001"; // строка
-    regex regular("1(0+)1"); // условие поиска
-    smatch result; // результат
-
-    while (regex_search(str, result, regular)) {
-        cout << result[0] << endl; // возвращает первое совпадение с регулярным выражением
-        str = result.suffix().str(); // возвращает оставшуюся часть строки после последнего соответствия регулярному выражению
+    string str = "sd10sds101sdd100101"; // строка
+    int in = 0;
+    string out ="";
+    for (int i = 0; i < str.length(); i++){
+        if(str[i]=='1' && in == 0){
+            in = 1;
+            out = '1';
+            continue; 
+        }
+        if(str[i]=='1' && in == 2){
+            out += '1';
+            cout << out << endl;
+            in = 0;
+            i--;
+        }
+        if (str[i]=='0' && (in == 1 || in == 2)){
+            out += '0';
+            in = 2;
+        }
+        else if(in == 1 || in == 2){
+            in = 0;
+        }
+        
     }
 
     return 0;
